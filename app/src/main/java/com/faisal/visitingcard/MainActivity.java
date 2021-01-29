c8ca38abc6fc561665ebe27d9461152ce724517d
Sk Faisal <faisal.hossain.pk@gmail.com>
2021-01-29 09:18:52 +0600

    dial pad integrated

--- app/src/main/java/com/faisal/visitingcard/MainActivity.java
+++ app/src/main/java/com/faisal/visitingcard/MainActivity.java
@@ -1,8 +1,11 @@
 package com.faisal.visitingcard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.AndroidViewModel;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.faisal.visitingcard.base.BaseActivity;
@@ -16,6 +19,17 @@ public class MainActivity extends BaseActivity {
     }
     @Override
     public void startUI() {
+        mainBinding=(ActivityMainBinding)getViewDataBinding();
+        goToDialPad();
 
+
+    }
+
+    private void goToDialPad() {
+        mainBinding.textViewNumber.setOnClickListener(View->{
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel:"+mainBinding.textViewNumber.getText().toString().trim()));
+            startActivity(callIntent);
+        });
     }
 }
